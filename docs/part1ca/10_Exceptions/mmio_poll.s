a7ca22a53ef8876c6a44c74cd854f18cb1cfaaac
Andrei Tatarnikov <andrewt0301@gmail.com>
2021-02-19 10:38:24 +0300

    up
    Signed-off-by: Andrei Tatarnikov <andrewt0301@gmail.com>

--- docs/part1ca/10_Exceptions/mmio_poll.s
+++ docs/part1ca/10_Exceptions/mmio_poll.s
@@ -2,16 +2,24 @@
 # Example: read a character from a keyboard and prints it to the diaplay
 #          by polling a MMIO device
   .text
-  .eqv CONTROL  0xffff0000
-  .eqv KEYBOARD 0xffff0004
-  .eqv DISPLAY  0xffff000C
+  .eqv IN_CTRL  0xffff0000
+  .eqv IN       0xffff0004
+  .eqv OUT_CTRL 0xffff0008
+  .eqv OUT      0xffff000C
 main:
-  li s0, CONTROL
-  li s1, KEYBOARD
-  li s2, DISPLAY
-poll_char:
-  lw t0, 0(s0)
-  beqz t0, poll_char
-  lw t0, 0(s1)
-  sw t0, 0(s2)
-  j poll_char
+  li   s0, IN_CTRL
+  li   s1, IN
+  li   s2, OUT_CTRL
+  li   s3, OUT
+
+poll_in:
+  lw   t0, 0(s0)
+  beqz t0, poll_in
+  lw   t0, 0(s1)
+
+poll_out:
+  lw   t1, 0(s2)
+  beqz t1, poll_out
+  sw   t0, 0(s3)
+
+  j poll_in
