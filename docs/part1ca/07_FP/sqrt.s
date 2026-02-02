45740c1a0c6f519bb65474e50c2a813b32821e86
Andrei Tatarnikov <andrewt0301@gmail.com>
2026-02-02 20:39:50 +0300

    Up
    Signed-off-by: Andrei Tatarnikov <andrewt0301@gmail.com>

--- docs/part1ca/07_FP/sqrt.s
+++ docs/part1ca/07_FP/sqrt.s
@@ -9,27 +9,28 @@ dst:
 idst:
     .word   0
 .text
-    la       t0, src    # source integer
+    la       t0, src      # source integer
     lw       t2, 0(t0)
-    fcvt.s.w f2, t2
-    flw      f0, 4(t0)
+    fcvt.s.w ft2, t2
+    flw      ft0, 4(t0)
 
-    flt.s    t2, f2, f0 # check if <0 ...
-    bnez     t2, nosqrt # no root then
-    fsqrt.s  f2, f2
+    flt.s    t2, ft2, ft0 # check if < 0 ...
+    bnez     t2, nosqrt   # no root then
+    fsqrt.s  ft2, ft2
+
 nosqrt:
-    fsw      f2, 4(t0)  # store float result
-    fcvt.w.s t1, f2     # convert to integer
-    sw       t1, 8(t0)  # store integer result
+    fsw      ft2, 4(t0)   # store float result
+    fcvt.w.s t1, ft2      # convert to integer
+    sw       t1, 8(t0)    # store integer result
 
-    li       a7, 2      # print float result
-    fmv.s    fa0, f2
+    li       a7, 2        # print float result
+    fmv.s    fa0, ft2
     ecall
 
-    li       a7, 11    # print integer result
+    li       a7, 11       # print new line
     li       a0, '\n'
     ecall
 
-    li       a7, 1
+    li       a7, 1        # print integer result
     mv       a0, t1
     ecall
