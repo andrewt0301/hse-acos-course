9e7730a1cec445ab53cc6bbf17d82051ff87382a
Andrei Tatarnikov <andrewt0301@gmail.com>
2026-02-26 18:24:04 +0300

    Up
    Signed-off-by: Andrei Tatarnikov <andrewt0301@gmail.com>

--- docs/part1ca/09_VM/PseudoVM.s
+++ docs/part1ca/09_VM/PseudoVM.s
@@ -1,31 +1,36 @@
 .globl	main
 .text
-main:	la	t1 handler
-	csrw	t1 utvec
-	csrsi	ustatus 1
-	
-loop:	li	a7 5		# Ввод адреса
-	ecall
-	beqz	a0 done		# Если 0, конец
-	andi	t0 a0 3		# Кратен ли 4
-	beqz	t0 read		# Если да — чтение
+main:
+    la      t1, handler
+    csrw    t1, utvec
+    csrsi   ustatus, 1
 
-write:	andi	a0 a0 -4	# Запись, затираем 2 бита
-	mv	t1 a0		# Это «виртуальный» адрес
-	li	a7 5		# Вводим значение
-	ecall
-	mv	t0 a0		# Готовим t0
-	sw	t0 (t1)		# Сохраняем знеачение в «виртуальной памяти»
-	b 	loop
+loop:
+    li      a7, 5       # Input address
+    ecall
+    beqz    a0, done    # If 0, then done
+    andi    t0, a0, 3   # Is address multiple of 4?
+    beqz    t0, read    # If yes, then read
 
-read:	lw	t0 (a0)		# Читаем значение из «виртуальной памяти»
-	mv	a0 t0
-	li	a7 1		# Выводим значение
-	ecall
-	li	a0 '\n'		# И перевод строки
-	li	a7 11
-	ecall
-	b 	loop
+write:
+    andi    a0, a0, -4  # Write, erase 2 lower bits of address
+    mv      t1, a0      # This is "virtual address"
+    li      a7, 5       # Input value
+    ecall
+    mv      t0, a0      # Prepate t0
+    sw      t0, (t1)    # Write a value to "virtual memory"
+    b       loop
 
-done:	li	a7 10
+read:
+    lw      t0, (a0)    # Read a value from "virtual memory"
+    mv      a0, t0
+    li      a7, 1       # Print the value
 	ecall
+    li      a0, '\n'    # Print new line
+    li      a7, 11
+    ecall
+    b       loop
+
+done:
+    li      a7, 10      # Exit
+    ecall
